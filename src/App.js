bc4a224cd6ef623c46725b7123ea5ed6140c0c72
Josh Rehanek <joshrehanek@gmail.com>
2021-04-03 00:14:09 -0700

    adds header, footer, & container to App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "./styles.css";
 
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Container from "./components/Container";
-import Input from "./components/Input";
-import EmployeeDetails from "./components/EmployeeDetails";
-import Row from "./components/Row";
-import Card from "./components/Card";
+// import SearchBox from "./components/SearchBox";
+// import EmployeeDetails from "./components/EmployeeDetails";
+
 
 
 const App = () => {
-  document.title = "Employee DIrectory";
+  document.title = "Employee Directory";
   return (
       <div>
+        <Header />
         <Container />
-        <Input />
-        
-        <Row />
+        <Footer />
       </div>
   );
 };
