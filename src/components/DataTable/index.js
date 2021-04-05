43212e6c6ed523bf94ce1d823bf12ed2a94b06fc
Josh Rehanek <joshrehanek@gmail.com>
2021-04-04 22:25:18 -0700

    reworks table w/ api call & search handlers

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,38 +1,64 @@
-import React from 'react'
-import './style.css'
+import React, { useState, useEffect } from 'react';
+import './style.css';
 import { Table } from 'react-bootstrap';
 
-export default function DataTable({ Employees }) {
+export default function DataTable({ search }) {
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        fetch("https://randomuser.me/api/?results=15&nat=us")
+            .then((res) => res.json())
+            .then((res) => {
+                setEmployees(res.results)
+            }
+            )
+    }, [])
+
+    const handleSearchContents = (string, match) =>
+        string.toLowerCase().indexOf(match.toLowerCase()) !== -1;
+
+    const handleSearchBoxChange = ({ name, email }) =>
+        !search ||
+        handleSearchContents(name.first, search) ||
+        handleSearchContents(name.last, search) ||
+        handleSearchContents(email, search);
+
     return (
-        <div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Location</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {this.Employees.map(function (users) {
-                    return(
-                    <tr>
-                    <td>
-                        <img src={users.picture.thumbnail} alt="profile"/>
-                    </td>
-                        <td>{users.name.first}</td>
-                        <td>{users.name.last}</td>
-                        <td>{users.email}</td>
-                        <td>{users.location.state}, {users.location.country}</td>
-                    </tr>
-                    );
-                })}
-                </tbody>
-            </Table>
-       
-                        {/* use .map function to map over fileteredEmployes for each user intaking props and make .row for each */}
-            
-        </div>
-    )
-}
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Location</th>
+                </tr>
+            </thead>
+            <tbody>
+                {employees
+                    .filter(handleSearchBoxChange)
+                    .map(({ name, email, location, picture }, i) =>
+                        <EmployeeRow
+                            name={ name }
+                            email={ email }
+                            location={ location }
+                            picture={ picture }
+                            i={i}
+                        />)}
+            </tbody>
+        </Table>
+    );
+
+
+function EmployeeRow({ name, email, location, picture }) {
+    return (
+        <tr>
+            <td>{ name.first }</td>
+            <td>{ name.last }</td>
+            <td>{ email }</td>
+            <td>{ location.city }, { location.state }</td>
+            <td><img src={ picture.thumbnail } alt="profile" /></td>
+        </tr>
+    );
+};
+};
+
